f8cbfb88fbd3ef98c979af786a0623dacc96f3d4
anatawa12 <anatawa12@icloud.com>
2025-06-28 13:54:37 +0900

    feat: add today card

---
+++ Assets/HistoryOfAvatarOptimizer/GenerateFolder/TodayCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+
+namespace HistoryOfAvatarOptimizer.ReleaseNoteCard
+{
+    public class TodayCard : UdonSharpBehaviour
+    {
+        public string epicDate = "2023-06-27";
+        public float dayLength = 0.02f;
+
+        void Start()
+        {
+            var epocDate = DateTime.Parse(epicDate);
+            var today = DateTime.Now;
+            var daysSinceEpic = (today - epocDate).TotalDays;
+            transform.localPosition = new Vector3((float)daysSinceEpic * dayLength, 0, 0);
+        }
+    }
+}
